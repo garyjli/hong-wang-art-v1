d60599d99681d795150648b78111d599ed4e0702
garyjli <ligaryj1@gmail.com>
2025-03-23 20:20:31 -0400

    added gallery.js

---
+++ gallery.js
@@ -0,0 +1,30 @@
+const imgFilenames = [];
+for (let i = 1; i <= 9; i++) {
+	imgFilenames.push(`a${i}.jpeg`);
+}
+
+function createGallery(imgFilenames) {
+	const box = document.querySelector(".box");
+	const filenames = Array.from({ length: 3 }, () => []);
+
+	imgFilenames.forEach((img, idx) => {
+		const columnNum = idx % 3;
+		filenames[columnNum].push(img);
+	});
+
+	filenames.forEach((column) => {
+		const columnDiv = document.createElement("div");
+		columnDiv.classList.add("dream");
+
+		column.forEach((image) => {
+			const img = document.createElement("img");
+			img.src = `images/${image}`;
+			img.onclick = () => openModal(`images/${image}`);
+			columnDiv.appendChild(img);
+		});
+
+		box.appendChild(columnDiv);
+	});
+}
+
+createGallery(imgFilenames);
