b44fd505c467d3308a6bfe1fb87cdc969a79736d
garyjli <ligaryj1@gmail.com>
2025-03-24 12:37:40 -0400

    sequential img load

--- gallery.js
+++ gallery.js
@@ -1,45 +1,3 @@
-// const imgFilenames = [];
-// for (let i = 1; i <= 86; i++) {
-// 	imgFilenames.push(`a${i}.jpeg`);
-// }
-
-// function createGallery(imgFilenames) {
-// 	const box = document.querySelector(".box");
-//     const filenames = Array.from({ length: 3 }, () => []);
-//     let height1 = 0, height2 = 0, height3 = 0;
-
-// 	imgFilenames.forEach((img, idx) => {
-//         const columnNum = idx % 3;
-        
-//         const imgElement = new Image();
-//         imgElement.src = `images/${img}`;
-    
-//         imgElement.onload = () => {
-//             const imgHeight = imgElement.height;
-//             console.log(`Image: ${img}, Height: ${imgHeight}px`);
-//         }
-
-// 		filenames[columnNum].push(img);
-// 	});
-
-// 	filenames.forEach((column) => {
-// 		const columnDiv = document.createElement("div");
-// 		columnDiv.classList.add("dream");
-
-// 		column.forEach((image) => {
-// 			const img = document.createElement("img");
-//             img.src = `images/${image}`;
-//             img.loading = "lazy";
-// 			img.onclick = () => openModal(`images/${image}`);
-// 			columnDiv.appendChild(img);
-// 		});
-
-// 		box.appendChild(columnDiv);
-// 	});
-// }
-
-// createGallery(imgFilenames);
-
 const imgFilenames = [];
 for (let i = 1; i <= 86; i++) {
 	imgFilenames.push(`a${i}.jpeg`);
@@ -57,24 +15,33 @@ for (let i = 0; i < 3; i++) {
 	box.appendChild(col);
 }
 
-// load images one by one
-imgFilenames.forEach((imgName) => {
+// used to load images sequentially
+let currentIndex = 0;
+
+function loadNextImage() {
+	if (currentIndex >= imgFilenames.length) return;
+
+	const imgName = imgFilenames[currentIndex];
 	const img = document.createElement("img");
 	img.src = `images/${imgName}`;
-	// img.loading = "lazy";
 	img.style.objectFit = "contain";
 	img.onclick = () => openModal(`images/${imgName}`);
 
-	// wait until image loads and renders
 	img.onload = () => {
-		// temporarily append to shortest column to get rendered height
+		// find shortest column
 		const minIndex = columnHeights.indexOf(Math.min(...columnHeights));
 		columns[minIndex].appendChild(img);
 
-		// after render, get the new height of this image
-		const renderedHeight = img.offsetHeight;
-		columnHeights[minIndex] += renderedHeight;
-	};
-});
+		// measure after render
+		requestAnimationFrame(() => {
+			const renderedHeight = img.offsetHeight;
+			columnHeights[minIndex] += renderedHeight;
+			console.log(`Image: ${imgName}, Height: ${renderedHeight}px`);
 
-// randomness is due to image loading at different times
+			currentIndex++;
+			loadNextImage();
+		});
+	};
+}
+
+loadNextImage();
